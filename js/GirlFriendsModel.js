ad1daf6bcdf8feb85d2c031164283b12ae92efec
Alexander Bespalko <alexander.bespalko@sigmaukraine.com>
2013-06-12 20:06:45 +0300

    VK

--- js/GirlFriendsModel.js
+++ js/GirlFriendsModel.js
@@ -19,70 +19,35 @@ function GirlFriendsViewModel() {
 	self.offset = 0;
 	self.count = 0;
 	self.friend_ids = [];
-	self.requests = 0;
-	self.errors = ko.observableArray([]);
+	self.errors = ko.observableArray([
 
-	self.relations = [
-		{ relation: "Standard (sandwich)", price: 0 }
-	];
-
-	self.vkLogin = function() {
-		/*FB.login(function(response) {
-			if (response.authResponse) {
-				console.log('Welcome!  Fetching your information.... ');
-				FB.api('/me', function(response) {
-				    console.log('Good to see you, ' + response.name + '.');
-				});
-			} else {
-				console.log('User cancelled login or did not fully authorize.');
-			}
-		});*/
-	};
-
-	self.getMoreFriends = function() {
-		
-		/*FB.api('/me/friends', {fields: 'id'}, function(response) {
-			if (!response || response.error) {
-				alert('Error occured');
-			} else {*/
-		FB.api('/me', function(response) {
-			console.log("Welcome " + response.name);
-
-				/*var uids = [];
-				ko.utils.arrayForEach(response.data, function(idx2,val2) {
-					uids.push(idx2.id);
-				});
-				uids = uids.join();*/
-				FB.api(
-					{
-						method: 'fql.query',
-						/*query: '' +
-							'SELECT uid, name, work_history  ' +
-							'FROM user WHERE uid IN (' +
-								'SELECT uid2 ' +
-								'FROM friend ' +
-								'WHERE uid1 IN (' +
-									'SELECT uid ' +
-									'FROM user ' +
-									'WHERE uid IN (' +
-										'SELECT uid2 ' +
-										'FROM friend ' +
-										'WHERE uid1 = ' + response.id + ' )))'*/
-						query: '' +
-							'SELECT uid2 ' +
-							'FROM friend WHERE uid1 = 1490938150'
-
-					},
-					function(data) {
-						var a = 1;
-					}
-				);
-		});
-	};
+	]);
 
 	self.friends = ko.observableArray([
 
 	]);
+
+	self.relations = [{
+		relation: "Standard (sandwich)", price: 0
+	}];
+
+	self.vkLogin = function() {
+		VK.Auth.login(authInfo, 1026);
+	};
+
+	self.getMoreFriends = function() {
+		VK.Api.call(
+			'friends.get',
+			{
+				fields: 'sex, photo',
+				count: self.count,
+				offset: self.offset,
+				order: 'name'
+			},
+			getUserProfileDataCallback
+		)
+	};
+
 
 	function getUserProfileDataCallback(data) {
 		if (!data.error) {
@@ -92,7 +57,7 @@ function GirlFriendsViewModel() {
 					var val = data.response[key];
 					if (val.sex == 1) {
 						self.friend_ids.push(val.uid);
-						VK.Api.call(
+						/*VK.Api.call(
 							'friends.get',
 							{
 								uid: val.uid,
@@ -100,11 +65,15 @@ function GirlFriendsViewModel() {
 								order: 'name'
 							},
 							getFriendsOfFriends
-						)
-						//self.friends.push(new UserProfile(val));
+						)*/
+						self.friends.push(new UserProfile(val));
+						if ((self.friends().length % 20) == 0) {
+							self.offset += parseInt(key);
+							return
+						}
 					}
 				}
-				self.offset += data.response.length;
+
 
 			}
 		}
@@ -131,7 +100,6 @@ function GirlFriendsViewModel() {
 	self.total_friends = function() {
 		return self.friends().length;
 	}
-
 
 }
 
